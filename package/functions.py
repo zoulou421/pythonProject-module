ac70330c2ec61c90a71e3a63f1faddad59ea58e1
Bonevy BEBY <bonevybeby@gmail.com>
2024-04-05 14:57:53 +0200

    added: package created in python

---
+++ package/functions.py
@@ -0,0 +1,8 @@
+"""multiply_module module containing the table function"""
+def table(nb, max=10):
+ """Function displaying the multiplication table by number of
+1 * nb up to max * n"""
+ i = 0
+ while i < max:
+   print(i + 1, "*", nb, "=", (i + 1) * nb)
+   i += 1
---
+++ test_package.py
@@ -0,0 +1,9 @@
+import os
+#import package.functions 1
+#from package import functions #2
+from package.functions import table #3
+#table function test
+#functions.table(3,20) #2
+#functions.table(3, 20) 1
+table(3,4)
+os.system("pause")
