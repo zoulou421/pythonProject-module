e4fc140079286b317519a3aa5d0d41612c585694
Bonevy BEBY <bonevybeby@gmail.com>
2024-04-05 17:18:25 +0200

    added: raise an exception using the raise keyword

--- assertions_test.py
+++ assertions_test.py
@@ -1,8 +1,9 @@
 year = input("Enter a year greater than 0 :")
 try:
- year = int(year) # Year conversion
- assert year > 0
+    year = int(year)  # Year conversion
+    if year <= 0:
+        raise ValueError("the year entered is negative or zero")
 except ValueError:
- print("You did not enter a number.")
-except AssertionError:
- print("The year entered is less than or equal to 0.")
+    print("You did not enter a number.")
+except ValueError:
+    print("The value entered is invalid (the year may benegative).")
