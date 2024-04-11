2f13390ac20250d9222c41408469565706a573d7
Bonevy BEBY <bonevybeby@gmail.com>
2024-04-11 23:44:35 +0200

    added: logging file

---
+++ logging-file.py
@@ -0,0 +1,10 @@
+import  logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+logging.debug("the function has been well executed")
+logging.info("Message for general information")
+logging.warning("Attention")
+logging.error("An error occurred")
+logging.critical("Critical error")
