947009f56cb712529f2a4c50ba08eccc9b49849f
Bonevy BEBY <bonevybeby@gmail.com>
2024-04-12 00:07:07 +0200

    added: logging file+format+write in a created file

--- logging-file.py
+++ logging-file.py
@@ -1,6 +1,8 @@
 import  logging
 
 logging.basicConfig(level=logging.DEBUG,
+                    filename="app.log",
+                    filemode="a",
                     format='%(asctime)s-%(levelname)s-%(message)s')
 
 
