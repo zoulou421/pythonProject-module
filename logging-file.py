e314e63cc43cec8d6da425fbe64d30e3772677e7
Bonevy BEBY <bonevybeby@gmail.com>
2024-04-11 23:53:37 +0200

    added: logging file+format

--- logging-file.py
+++ logging-file.py
@@ -1,6 +1,7 @@
 import  logging
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s-%(levelname)s-%(message)s')
 
 
 logging.debug("the function has been well executed")
