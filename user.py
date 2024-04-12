ddefb4fb39a482dfbfc476cf5105fd675b70270c
Bonevy BEBY <bonevybeby@gmail.com>
2024-04-12 08:40:33 +0200

    added: fake users table functions

--- user.py
+++ user.py
@@ -7,11 +7,25 @@ def get_user():
     return f"{fake.first_name()} {fake.last_name()}"
 
 def get_users(n):
-    pass
+    users = []
+    for i in range(n):
+        users.append(get_user())
+
+    return  users
+
+def get_users_v2(n):
+    users = []
+    for _ in range(n): #i replace with _
+        users.append(get_user())
+
+    return  users
+def get_users_v3(n):
+    return  [get_user() for _ in range(n)]
+
 
 if __name__=="__main__":
-    user=get_user()
-    print(user)
+    users=get_users(n=5)
+    print(users)
 
 
 
